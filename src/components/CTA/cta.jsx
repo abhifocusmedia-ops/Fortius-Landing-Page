dbf7cc604992abea04d81791fd05b5c3ed210171
abhifocusmedia-ops <abhifocusmedia@gmail.com>
2026-06-11 20:07:52 +0530

    map updated

--- src/components/CTA/cta.jsx
+++ src/components/CTA/cta.jsx
@@ -60,7 +60,7 @@ function CTA({ setShowModal}) {
 
             <iframe
               title="location"
-              src="https://www.google.com/maps?q=Hyderabad&output=embed"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3303.337520056136!2d-84.22125592424153!3d34.112107973134435!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88f59d871f9d5b19%3A0xa247670808ae3f21!2sFortius%20Sports%20Academy%20%40GG!5e0!3m2!1sen!2sin!4v1781188566754!5m2!1sen!2sin"
               loading="lazy"
             ></iframe>
 
